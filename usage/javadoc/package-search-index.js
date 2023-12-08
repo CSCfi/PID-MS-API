65807f0969672e79778eb69f76ca1c592b9b23c4
Pekka Järveläinen <pj@csc.fi>
2023-12-08 14:36:09 +0200

    javadoc

---
+++ usage/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fi.csc.pid.api"},{"l":"fi.csc.pid.api.db"},{"l":"fi.csc.pid.api.doi"},{"l":"fi.csc.pid.api.entity"},{"l":"fi.csc.pid.api.hallinta"},{"l":"fi.csc.pid.api.handle"},{"l":"fi.csc.pid.api.model"},{"l":"fi.csc.pid.api.model.doi"},{"l":"fi.csc.pid.api.service"}];updateSearchResults();
